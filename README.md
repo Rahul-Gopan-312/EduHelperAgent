6cf912d2baf78101b21dd08bf784ebe287506ce6
Rahul Gopan <rahulgopan@smartlounge.online>
2026-05-01 12:53:27 +0530

    Updating Route and Readme

--- README.md
+++ README.md
@@ -82,7 +82,7 @@ php artisan migrate
 php artisan serve
 ```
 
-Visit **http://127.0.0.1:8000/chat**
+Visit **http://127.0.0.1:8000/**
 
 ---
 
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ChatController;
 
-Route::get('/chat', function () {
+Route::get('/', function () {
     return view('chat');
 });
 
